892ede49f78b7fc0ed4e2c95a0f872eccad9a14c
oandrade <oandrade@aplose.fr>
2024-10-23 16:27:49 +0200

    Translation management update

--- projects/aplose-framework-ng/src/lib/pipe/i18n.pipe.ts
+++ projects/aplose-framework-ng/src/lib/pipe/i18n.pipe.ts
@@ -8,9 +8,8 @@ import { I18nService } from '../service/i18n.service';
 export class I18nPipe implements PipeTransform {
   private i18nService:I18nService=inject(I18nService);
 
-  transform(translationKey: string, ...args: unknown[]): unknown {
-//    this.i18nService.getT
-    return null;
+  transform(translationKey: string, defaultValue: string): Promise<string> {
+    return this.i18nService.getTranslation(translationKey, defaultValue);
   }
 
 }
